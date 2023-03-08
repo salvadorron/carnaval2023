67b9417e8222755be00cc489372b1843608814d6
Salvador Ron <salvadorronosio@gmail.com>
2023-03-07 23:45:40 -0400

    Add id to appbar  and delete unused elements

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import banner from '../assets/img/banner.png';
-import '../assets/css/flag.css'
 
 //const lightColor = 'rgba(255, 255, 255, 0.7)';
 function Header(props) {
@@ -10,8 +9,7 @@ function Header(props) {
 
   return (
     <React.Fragment>
-      <AppBar color="transparent" position="relative" elevation={0} style={{background: 'url('+ banner +') no-repeat center / 35%  ', height: '8em'}}>
-        <div className='flag'></div>
+      <AppBar id="appbar" color="transparent" position="relative" elevation={0} style={{background: 'url('+ banner +') no-repeat center / 35%  ', height: '8em'}}>
       </AppBar>
     </React.Fragment>
   );
