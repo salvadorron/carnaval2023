a61df58fd1153d07e0b0248ae1f9b9207b14555f
Salvador Ron <salvadorronosio@gmail.com>
2023-03-08 15:06:17 -0400

    Update report to pdf

--- client/src/components/PDFDocument.js
+++ client/src/components/PDFDocument.js
@@ -4,7 +4,7 @@ import html2canvas from 'html2canvas';
 
 async function PDFDocument (activity) {
   
-  await html2canvas(document.getElementById('appbar')).then(function(canvas){
+  await html2canvas(document.getElementById('img')).then(function(canvas){
     const doc = new jsPDF();
 
   // define the columns we want and their titles
@@ -36,15 +36,20 @@ async function PDFDocument (activity) {
     // we use a date string to generate our filename.
     const dateStr = date[0] + date[1] + date[2];
     // ticket title. and margin-top + margin-left
-    const width = doc.internal.pageSize.getWidth();
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const widthRatio = pageWidth / canvas.width;
+    
+    const canvasWidth = canvas.width * widthRatio;
+
+    const marginX = (pageWidth - canvasWidth) / 2;
 
     const imgData = canvas.toDataURL('image/png');
-    doc.addImage(imgData, 'PNG', -15, 2);
+    doc.addImage(imgData, 'PNG', marginX, 0);
     doc.setFont('Helvetica', 'normal')
     doc.setFontSize(24);
-    doc.text("Actividades Desarrolladas", width/2, 45, {align: 'center'});
+    doc.text("Actividades Desarrolladas", pageWidth/2, 45, {align: 'center'});
     doc.setFontSize(10);
-    doc.text(`© Todos los derechos reservados Ven Guarico Tu Destino ${new Date().getFullYear()}.`, width/2, tableY+15, {align: 'center'});
+    doc.text(`© Todos los derechos reservados Ven Guarico Tu Destino ${new Date().getFullYear()}.`, pageWidth/2, tableY+15, {align: 'center'});
     // we define the name of our PDF file.
     doc.save(`reporte_${dateStr}.pdf`);
   })
